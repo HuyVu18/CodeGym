f01d12e81911e9d0b178f56b7061c5bfb7827646
Chung Phung Phat <chphungphat@gmail.com>
2023-03-09 15:33:42 +0700

    add

--- module-01/sources/lesson-08/js/bai_3.js
+++ module-01/sources/lesson-08/js/bai_3.js
@@ -1,12 +1,3 @@
-// let value = prompt('Type a number', 0);
-// if (value > 0) {
-//     alert(1);
-// } else if (value < 0) {
-//     alert(-1);
-// } else {
-//     alert(0);
-// }
-
 let value = prompt("Type a number");
 switch(true) {
     case (value > 0):
