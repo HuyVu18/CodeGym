b604aaf708c6ad438649f1f33fe95702fcb941b0
Chung Phùng Phát <chphungphat@gmail.com>
2023-03-16 17:01:01 +0700

    add

--- module-01/sources/lesson-07/js/CurrencyConversion.js
+++ module-01/sources/lesson-07/js/CurrencyConversion.js
@@ -1,8 +1,8 @@
 function Conversion() {
     const ratio = 25000;
-    var inamount = document.getElementById("amount").value;
-    var incur = document.getElementById("inputCurrency").value;
-    var outcur = document.getElementById("outputCurrency").value;
+    let inamount = document.getElementById("amount").value;
+    let incur = document.getElementById("inputCurrency").value;
+    let outcur = document.getElementById("outputCurrency").value;
     if (incur == "VND" && outcur == "USD")
     {
         let result = inamount/ratio;
