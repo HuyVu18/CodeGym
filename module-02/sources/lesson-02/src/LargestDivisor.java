979dbc2490e453c5a976738bd68a0792fec274b3
Reyon <chphungphat@gmail.com>
2023-04-04 15:17:56 +0700

    20 prime number

--- module-02/sources/lesson-02/src/LargestDivisor.java
+++ module-02/sources/lesson-02/src/LargestDivisor.java
@@ -7,6 +7,7 @@ public class LargestDivisor {
         }
         return gcd(b % a, a);
     }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter a: ");
